0a1007bb8a52a87c296b21a6c22f5133c16ed7ee
muycul <48526137+muycul@users.noreply.github.com>
2021-08-25 16:32:18 -0500

    Create cul-woocommerce-list-info.php

---
+++ cul-woocommerce-list-info.php
@@ -0,0 +1,15 @@
+<?php
+
+	/**
+	 * A Plugin that shows all related pending orders to a subscrition to double check credit reporting
+	 *
+	 * @package cul-woocommerce-list-info
+	 *
+	 * Plugin Name:       CUL - Extra information in admin woocommerce lists
+	 * Description:       Plugin that shows extra information in orders and subscription lists in the admin panel
+	 * Version:           1.0
+	 * Author:            CUL
+	 */
+
+
+	
